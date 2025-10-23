2e9415e84c09c9995bfb0232b86bdbff406b8125
Amit Shahani <amit.shahani11@2gmail.com>
2025-10-23 10:17:02 +0530

    Use correct gunicorn module binding in Procfile

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\lenovo\desktop\python\vidly\venv\scripts\python.exe
+#!C:\Amit\project_vidly-master\project_vidly\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
